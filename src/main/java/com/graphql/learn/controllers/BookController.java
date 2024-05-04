8ef7f1041cb0e6dd2b329a8f3df1832f192d2842
Abhimanyu Dwivedi <19746098+ABinfinity@users.noreply.github.com>
2024-05-04 21:25:27 +0530

    updated to graphQL

--- src/main/java/com/graphql/learn/controllers/BookController.java
+++ src/main/java/com/graphql/learn/controllers/BookController.java
@@ -3,34 +3,56 @@ package com.graphql.learn.controllers;
 import com.graphql.learn.entities.Book;
 import com.graphql.learn.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+import lombok.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/books")
+@Controller
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
     //create
-    @PostMapping("/createBook")
-    public Book create(@RequestBody Book book){
-        return this.bookService.create(book);
+    @MutationMapping("createBook")
+    public Book create(@Argument BookInput book){
+        Book b = new Book();
+        b.setTitle(book.getTitle());
+        b.setPrice(book.getPrice());
+        b.setPages(book.getPages());
+        b.setAuthor(book.getAuthor());
+        b.setDesc(book.getDesc());
+
+        return this.bookService.create(b);
     }
 
 
     //getAll books
-    @GetMapping("/getAllBooks")
+    @QueryMapping("allBooks")
     public List<Book> getAll(){
         return this.bookService.getAll();
     }
 
 
     //get single book
-    @GetMapping("/getBook/{bookId}")
-    public Book getOne(@PathVariable int bookId){
+    @QueryMapping("getBook")
+    public Book getOne(@Argument int bookId){
         return this.bookService.get(bookId);
     }
+}
+
+
+@Getter
+@Setter
+class BookInput{
+    private String title;
+    private String desc;
+    private String author;
+    private double price;
+    private int pages;
+
 }
